386ccddc49caacd7cb3a0ead8f81bd3cbe43b980
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-29 21:16:42 +0200

    Create TrackViewer

---
+++ src/components/Views/Player/TrackViewer/TrackViewer.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="track-viewer">
+		<section class="track-viewer__info">
+			<h2 class="track-viewer__artist">Icona Pop</h2>
+			<h2 class="track-viewer__title">We got the World</h2>
+		</section>
+
+		<div class="fading"></div>
+	</div>
+</template>
+
+<script src="./component.js"></script>
+<style src="./style.sass" lang="sass"/>
