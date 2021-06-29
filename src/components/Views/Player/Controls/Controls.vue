c409f02575c80df12d5fd882c4e2bc0a60c84d73
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-29 21:14:30 +0200

    Create basic Controls component

---
+++ src/components/Views/Player/Controls/Controls.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="controls">
+		<div class="controls__content">
+			<div class="controls__tile">
+				<a href="#" class="controls__button controls__link" target="_blank">
+					<i class="fas fa-share-alt"></i>
+				</a>
+			</div>
+
+			<div class="controls__tile">
+				<button class="controls__button prev">
+					<i class="fas fa-step-backward"></i>
+				</button>
+
+				<button class="controls__button play" v-if="!play">
+					<i class="fas fa-play"></i>
+
+					<Timeline></Timeline>
+				</button>
+
+				<button class="controls__button pause" v-else>
+					<i class="fas fa-pause"></i>
+				</button>
+
+				<button class="controls__button next">
+					<i class="fas fa-step-forward"></i>
+				</button>
+			</div>
+
+			<div class="controls__tile">
+				<button class="controls__button favourite">
+					<i class="fas fa-heart"></i>
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script src="./component.js"></script>
+<style src="./style.sass" lang="sass"/>
---
+++ src/components/Views/Player/Controls/component.js
@@ -0,0 +1,8 @@
+import Timeline from '../Timeline/Timeline'
+
+export default {
+	name: 'Controls',
+	components: {
+		Timeline,
+	}
+}
