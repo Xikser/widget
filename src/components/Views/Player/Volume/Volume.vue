bbc6f3a1e013a7e76dcf1c00497aeee123cbe478
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-29 21:16:56 +0200

    Create Volume component

---
+++ src/components/Views/Player/Volume/Volume.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="volume">
+		<div class="volume__bar">
+			<div class="volume__bar-hover"></div>
+		</div>
+		<i class="fas fa-volume-up volume__icon"></i>
+	</div>
+</template>
+
+<script src="./component.js"></script>
+<style src="./style.sass" lang="sass"/>
