027876eef5b106c1960b4a9275520cbe8039c036
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-29 21:16:26 +0200

    Create top controls component

---
+++ src/components/Views/Player/TopNav/TopNav.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="top-nav">
+		<div class="top-nav__content">
+			<div class="top-nav__control-icons">
+				<i class="fas fa-retweet"></i>
+				<i class="fas fa-random"></i>
+				<i class="fas fa-redo"></i>
+			</div>
+		</div>
+
+		<button class="navbar">
+			<i class="fas fa-bars"></i>
+		</button>
+	</div>
+</template>
+
+<script src="./component.js"></script>
+<style src="./style.sass" lang="sass"/>
