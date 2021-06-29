101fac723e2b178064161ef1c3ab3ded535285cf
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-29 21:15:27 +0200

    Create basic Player component view

---
+++ src/components/Views/Player/Player.vue
@@ -0,0 +1,15 @@
+<template>
+	<section class="player">
+		<div class="player__top-content">
+			<TopNav></TopNav>
+			<TrackViewer></TrackViewer>
+		</div>
+		<div class="player__bottom-content">
+			<Volume></Volume>
+			<Controls></Controls>
+		</div>
+	</section>
+</template>
+
+<script src="./component.js"></script>
+<style src="./style.sass" lang="sass"/>
---
+++ src/components/Views/Player/component.js
@@ -0,0 +1,14 @@
+import Controls from './Controls/Controls'
+import Volume from './Volume/Volume'
+import TopNav from './TopNav/TopNav'
+import TrackViewer from './TrackViewer/TrackViewer'
+
+export default {
+	name: 'Player',
+	components: {
+		Controls,
+		Volume,
+		TopNav,
+		TrackViewer,
+	}
+}
