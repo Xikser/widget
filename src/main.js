d8ac29a216ea8eef32aaa73df1b414afd136f000
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-30 12:27:38 +0200

    Rename Playlist -> Tracklist

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import router from './router'
 import store from './store'
 
 import Player from './components/Views/Player/Player'
-import PlayList from './components/Views/TrackList/TrackList'
+import TrackList from './components/Views/TrackList/TrackList'
 
 createApp(App)
 	.component('Player', Player)
-	.component('PlayList', PlayList)
+	.component('TrackList', TrackList)
 	.use(store)
 	.use(router)
 	.mount('body')
